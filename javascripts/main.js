e119fb47562f625cf904244aa03c9e186b75d7c8
Marc Simonton <marcasimonton@gmail.com>
2017-05-12 21:44:24 -0600

    fade in is okay

--- javascripts/main.js
+++ javascripts/main.js
@@ -40,4 +40,21 @@ angRoute.config(function($stateProvider, $urlRouterProvider, $locationProvider) 
 
 $(function () {
   $('[data-toggle="popover"]').popover()
+
+
+
+  $(document).ready(function(){
+    $("button").click(function(){
+        var div = $("#logo-animation");
+        var bod = $('body');
+
+        div.animate({height: '400px', width:'660px', opacity: '1'}, 300);
+
+        div.animate({height: '340px', width: '550px',opacity: '.7'},400);
+        bod.stop().delay(200).animate({backgroundColor: '#26333c'}, 100);
+        bod.animate({backgroundColor: '#192228'},300);
+        div.animate({height: '351px', width: '581px',opacity: '1'}, 400);
+
+    });
+});
 })
